0b3ba3b8da582b253c4d2dc003a64e1658a1c47e
Nurlan <nurlan.grachyov@mail.ru>
2024-09-08 14:18:53 +0400

    add the time sorting function

--- src/processing.py
+++ src/processing.py
@@ -6,6 +6,11 @@ def filter_by_state(start_list: list[dict[str, object]], state: str = "EXECUTED"
     return list
 
 
+def sort_by_date(list_d: list[dict[str, object]], reverse: bool = True) -> list[dict[str, object]]:
+    sorted_list = sorted(list_d, key=lambda x: x["date"], reverse=reverse)
+    return sorted_list
+
+
 if __name__ == "__main__":
     """"""
     lst = [
@@ -15,3 +20,12 @@ if __name__ == "__main__":
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
     print(filter_by_state(lst))
+
+    """"""
+    list_date = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    print(sort_by_date(list_date))
