d99278780acf7931b9589b25cba8ecc9decd3c7e
Nurlan <nurlan.grachyov@mail.ru>
2024-09-08 14:16:36 +0400

    add list with state = executed

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,3 @@
-lst = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-
 def filter_by_state(start_list: list[dict[str, object]], state: str = "EXECUTED") -> list[dict[str, object]]:
     list = []
     for i in start_list:
@@ -14,4 +6,12 @@ def filter_by_state(start_list: list[dict[str, object]], state: str = "EXECUTED"
     return list
 
 
-print(filter_by_state(lst))
+if __name__ == "__main__":
+    """"""
+    lst = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    print(filter_by_state(lst))
