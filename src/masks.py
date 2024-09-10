7a90e94654704f43a71222ed13f8aff3e6342831
Nurlan <nurlan.grachyov@mail.ru>
2024-09-10 22:08:50 +0400

    add filtered_data

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,19 @@
 def get_mask_card_number(card: str) -> str:
     """Возвращает реквизиты карты с зашифрованным номером"""
-    slice_card = card[-10:-4]
-    mask_card = card.replace(slice_card, "******")
-    new_mask_card = mask_card[:-12] + " " + mask_card[-12:-8] + " " + mask_card[-8:-4] + " " + mask_card[-4:]
+    if len(card) == 16 and card.isdigit():
+        slice_card = card[-10:-4]
+        mask_card = card.replace(slice_card, "******")
+        new_mask_card = mask_card[:-12] + " " + mask_card[-12:-8] + " " + mask_card[-8:-4] + " " + mask_card[-4:]
+    else:
+        return 'Некорректный ввод'
     return new_mask_card
 
 
 def get_mask_account(acc: str) -> str:
     """Возвращает реквизиты счета с зашифрованным номером"""
-    slice_acc = acc[-20:-4]
-    mask_acc = acc.replace(slice_acc, "**")
+    if len(acc) == 20 and acc.isdigit():
+        slice_acc = acc[-20:-4]
+        mask_acc = acc.replace(slice_acc, "**")
+    else:
+        return 'Некорректный ввод'
     return mask_acc
