b63d48ab686f553026266c4d593fd9b2e9699072
Nurlan <nurlan.grachyov@mail.ru>
2024-09-14 15:11:11 +0400

    use parser in get_date

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# if len(date) == 26 and date[8:10].isdigit() and date[5:7].isdigit() and date[:4].isdigit():
+#     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+# else:
+#     return "Некорректная дата"
