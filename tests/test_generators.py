77ff8224448d96c3534bf6047abb5dd8ef658995
Nurlan <nurlan.grachyov@mail.ru>
2024-09-15 17:16:41 +0400

    исправил выходные данные 2го теста, ошибка сохраняется.

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -20,7 +20,7 @@ def test_filter_by_currency(input_currency, exit_currency):
                     "id": 939719570,
                     "state": "EXECUTED",
                     "date": "2018-06-30T02:08:58.425572",
-                    "operationAmount": {"amount": "9824.07", "currency": {"name": "EUR", "code": "USD"}},
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
                     "description": "Перевод организации",
                     "from": "Счет 75106830613657916952",
                     "to": "Счет 11776614605963066702",
@@ -35,17 +35,8 @@ def test_filter_by_currency(input_currency, exit_currency):
                     "to": "Счет 75651667383060284188",
                 },
             ],
-            [
-                {
-                    "id": 142264268,
-                    "state": "EXECUTED",
-                    "date": "2019-04-04T23:20:05.206878",
-                    "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-                    "description": "Перевод со счета на счет",
-                    "from": "Счет 19708645243227258542",
-                    "to": "Счет 75651667383060284188",
-                },
-            ],
+            "Перевод организации",
+            "Перевод со счета на счет",
         ),
     ],
 )
