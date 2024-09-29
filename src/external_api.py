0f461ed4204e4ea52e270905b4dadfd6a052d04a
Nurlan <nurlan.grachyov@mail.ru>
2024-09-29 19:15:52 +0400

    Добавил универсальный путь.

--- src/external_api.py
+++ src/external_api.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
 from typing import Any
 
 import requests
 from dotenv import load_dotenv
 
+PATH_TO_PROJECT = Path(__file__).resolve().parent.parent
+PATH_TO_FILE = PATH_TO_PROJECT / "data" / "operations.json"
 
 def conversion_currency(to_curr: str, from_curr: str, amount: str) -> Any:
     """Функция, конвертирующая сумму из иностранной валюты в рубли"""
@@ -12,13 +15,12 @@ def conversion_currency(to_curr: str, from_curr: str, amount: str) -> Any:
     url =  f"https://api.currencylayer.com/convert?from={from_curr}&to={to_curr}&amount={amount}?access_key={access_key}"
 
 
-
     headers = {"access_key": "access_key=" + access_key}
 
     response = requests.get(url)
 
     result = response.json()
-    return result
+    return result['result']
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,12 @@
 import json
+import os
+from pathlib import Path
 from typing import Any
 
 from src.external_api import conversion_currency
+
+PATH_TO_PROJECT = Path(__file__).resolve().parent.parent
+PATH_TO_FILE = PATH_TO_PROJECT / "data" / "operations.json"
 
 
 def get_transactions(file: str) -> Any:
@@ -18,19 +23,20 @@ def get_transactions(file: str) -> Any:
 
 def get_sum() -> Any:
     """Функция, суммирующая суммы транзакций в рублях"""
-    data = get_transactions("C:/Users/Nurlan/IT/Проекты/mask_card2/data/operations.json")
+    data = get_transactions(os.path.abspath(PATH_TO_FILE))
     lst_str = []
     amount = []
     for i in data:
         if len(i) == 0:
             continue
+        elif i["operationAmount"]["currency"]["code"] == "RUB":
+            lst_str.append(i["operationAmount"]["amount"])
         elif i["operationAmount"]["currency"]["code"] != "RUB":
             conv_curr = conversion_currency(
                 "RUB", i["operationAmount"]["currency"]["code"], i["operationAmount"]["amount"]
             )
             amount.append(conv_curr)
-        else:
-            lst_str.append(i["operationAmount"]["amount"])
+
     for i in lst_str:
         amount.append(float(i))
     total_amount = sum(amount)
@@ -38,5 +44,5 @@ def get_sum() -> Any:
 
 
 if __name__ == "__main__":
-    print(get_transactions("C:/Users/Nurlan/IT/Проекты/mask_card2/data/operations.json"))
-    print(get_sum())
+    print(get_transactions(os.path.abspath(PATH_TO_FILE)))
+
