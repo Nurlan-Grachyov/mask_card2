a24705fcbf5addcd26fbab07f2f1ad3284040ff7
Nurlan <nurlan.grachyov@mail.ru>
2024-09-29 12:22:28 +0400

    what`s wrong with get_sum?

--- src/external_api.py
+++ src/external_api.py
@@ -9,15 +9,16 @@ def conversion_currency(to_curr: str, from_curr: str, amount: str) -> Any:
     """Функция, конвертирующая сумму из иностранной валюты в рубли"""
     load_dotenv()
     access_key = os.getenv("access_key")
-    url =  'https://api.tinkoff.ru/v1/currency_rates?from=USD&to=RUB'
+    url =  f"https://api.currencylayer.com/convert?from={from_curr}&to={to_curr}&amount={amount}?access_key={access_key}"
 
 
-    headers = {"accept": "application/json"}
+
+    headers = {"access_key": "access_key=" + access_key}
 
     response = requests.get(url)
 
     result = response.json()
-    return result['result']
+    return result
 
 
 if __name__ == "__main__":
