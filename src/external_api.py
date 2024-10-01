ca1e9f81209e2d573da64c99f2641609b595be87
Nurlan <nurlan.grachyov@mail.ru>
2024-10-01 19:03:13 +0400

    ниже error не выводится.

--- src/external_api.py
+++ src/external_api.py
@@ -25,18 +25,15 @@ def conversion_currency(transaction) -> float:
 
 if __name__ == "__main__":
     print(
-        conversion_currency({
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  }))
+        conversion_currency(
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            }
+        )
+    )
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from pathlib import Path
 from typing import Any
@@ -6,15 +7,33 @@ from typing import Any
 PATH_TO_PROJECT = Path(__file__).resolve().parent.parent
 PATH_TO_FILE = PATH_TO_PROJECT / "data" / "operations.json"
 
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+fileHandler = logging.FileHandler(PATH_TO_PROJECT / "logs" / "utils.log", encoding="UTF-8", mode="w")
+fileFormatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+fileHandler.setFormatter(fileFormatter)
+logger.addHandler(fileHandler)
+
+
+logger_mistake = logging.getLogger("utils")
+logger_mistake.setLevel(logging.ERROR)
+fileHandler_mistake = logging.FileHandler(PATH_TO_PROJECT / "logs" / "utils.log", encoding="UTF-8", mode="w")
+fileFormatter_mistake = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+fileHandler_mistake.setFormatter(fileFormatter_mistake)
+logger_mistake.addHandler(fileHandler_mistake)
+
 
 def get_transactions(file: str) -> Any:
     """Функция, возвращающая данные из файла json"""
     with open(file, encoding="UTF-8") as f:
         try:
+            logger.info("Получаем данные json файла")
             data = json.load(f)
         except json.decoder.JSONDecodeError:
+            logger_mistake.error("Ошибка! Некорректные данные json файла")
             return []
         if len(data) == 0 or type(data) is not list:
+            logger_mistake.error("Ошибка! Либо файла пустой, либо файл не содержит список")
             return []
         return data
 
