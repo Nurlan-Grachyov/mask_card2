0561f24c5a3cedca4c0da8d3d14ff2bed4756f8e
Nurlan <nurlan.grachyov@mail.ru>
2024-10-05 19:06:21 +0400

    Починили

--- tests/test_files.py
+++ tests/test_files.py
@@ -6,7 +6,7 @@ from src.files import read_file_csv, read_excel
 @patch("src.files.pd.read_csv")  # Подменяем pd.read_csv
 def test_read_file_csv_success(mock_read_csv):
     mock_read_csv.return_value = Mock(to_dict=Mock(return_value={"col1": [1, 2], "col2": [3, 5]}))
-    result = read_file_csv()
+    result = read_file_csv("../files/file.csv")
     assert result == {"col1": [1, 2], "col2": [3, 5]}
 
 
