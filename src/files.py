a0b7132f7f360c81878d09622667d2889343c923
Nurlan <nurlan.grachyov@mail.ru>
2024-10-05 17:04:17 +0400

    чиним...

--- src/files.py
+++ src/files.py
@@ -1,41 +1,45 @@
-# import logging
-#
+import logging
+
 import pandas as pd
-#
-# from src.utils import PATH_TO_PROJECT
-#
-# PATH_TO_CSV = PATH_TO_PROJECT / "files" / "transactions.csv"
-# PATH_TO_EXCEL = PATH_TO_PROJECT / "files" / "transactions_excel.xlsx"
-#
-# logger = logging.getLogger("files")
-# logger.setLevel(logging.INFO)
-# fileHandler = logging.FileHandler(PATH_TO_PROJECT / "logs" / "files.log", encoding="UTF-8", mode="w")
-# fileFormatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
-# fileHandler.setFormatter(fileFormatter)
-# logger.addHandler(fileHandler)
+
+from src.utils import PATH_TO_PROJECT
+
+PATH_TO_CSV = PATH_TO_PROJECT / "files" / "transactions.csv"
+PATH_TO_EXCEL = PATH_TO_PROJECT / "files" / "transactions_excel.xlsx"
+
+logger = logging.getLogger("files")
+logger.setLevel(logging.INFO)
+fileHandler = logging.FileHandler(PATH_TO_PROJECT / "logs" / "files.log", encoding="UTF-8", mode="w")
+fileFormatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+fileHandler.setFormatter(fileFormatter)
+logger.addHandler(fileHandler)
 
 
 def read_file_csv(file='../files/file.csv'):
     try:
-        # logger.info("Получаем данные файла")
+        logger.info("Получаем данные файла")
         reader = pd.read_csv(file)
         list_reader = reader.to_dict()
         return list_reader
     except Exception:
-        # logger.error("Ошибка")
+        logger.error("Ошибка!")
         return []
+
+
 print(read_file_csv())
 
 
-# def read_excel(file):
-#     try:
-#         data_excel = pd.read_excel(file)
-#         dict_data = data_excel.to_dict()
-#         return dict_data
-#     except Exception:
-#         return []
+def read_excel(file):
+    try:
+        logger.info("Получаем данные файла")
 
+        data_excel = pd.read_excel(file)
+        dict_data = data_excel.to_dict()
+        return dict_data
+    except Exception:
+        logger.error("Ошибка!")
 
+        return []
 
 
 
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 
-from src.files import func, read_file_csv
+from pandas import read_excel
+
+from src.files import read_file_csv
 
 
 @patch('src.files.func')
@@ -9,46 +11,46 @@ def test_read_file_csv(mock):
     assert read_file_csv() == {'name' : 'Nurlan'}
 
 
-# @patch("src.files.read_excel")
-# def test_read_file_excel(mock_return):
-#     mock_return.return_value = {
-#         "League": {
-#             0: "English Premier League (1)",
-#             1: "Spain Primera Division (1)",
-#             2: "English Premier League (1)",
-#             3: "German 1. Bundesliga (1)",
-#             4: "Spain Primera Division (1)",
-#             5: "Italian Serie A (1)",
-#         },
-#         "Name": {
-#             0: "Manchester City",
-#             1: "Real Madrid",
-#             2: "Liverpool",
-#             3: "FC Bayern München",
-#             4: "FC Barcelona",
-#             5: "Juventus",
-#         },
-#         "TransferBudget": {0: 176000000, 1: 188500000, 2: 90000000, 3: 100000000, 4: 180500000, 5: 105000000},
-#         "Unnamed: 0": {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5},
-#     }
-#
-#     assert read_excel("../files/file.xlsx") == {
-#         "League": {
-#             0: "English Premier League (1)",
-#             1: "Spain Primera Division (1)",
-#             2: "English Premier League (1)",
-#             3: "German 1. Bundesliga (1)",
-#             4: "Spain Primera Division (1)",
-#             5: "Italian Serie A (1)",
-#         },
-#         "Name": {
-#             0: "Manchester City",
-#             1: "Real Madrid",
-#             2: "Liverpool",
-#             3: "FC Bayern München",
-#             4: "FC Barcelona",
-#             5: "Juventus",
-#         },
-#         "TransferBudget": {0: 176000000, 1: 188500000, 2: 90000000, 3: 100000000, 4: 180500000, 5: 105000000},
-#         "Unnamed: 0": {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5},
-#     }
+@patch("src.files.read_excel")
+def test_read_file_excel(mock_return):
+    mock_return.return_value = {
+        "League": {
+            0: "English Premier League (1)",
+            1: "Spain Primera Division (1)",
+            2: "English Premier League (1)",
+            3: "German 1. Bundesliga (1)",
+            4: "Spain Primera Division (1)",
+            5: "Italian Serie A (1)",
+        },
+        "Name": {
+            0: "Manchester City",
+            1: "Real Madrid",
+            2: "Liverpool",
+            3: "FC Bayern München",
+            4: "FC Barcelona",
+            5: "Juventus",
+        },
+        "TransferBudget": {0: 176000000, 1: 188500000, 2: 90000000, 3: 100000000, 4: 180500000, 5: 105000000},
+        "Unnamed: 0": {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5},
+    }
+
+    assert read_excel("../files/file.xlsx") == {
+        "League": {
+            0: "English Premier League (1)",
+            1: "Spain Primera Division (1)",
+            2: "English Premier League (1)",
+            3: "German 1. Bundesliga (1)",
+            4: "Spain Primera Division (1)",
+            5: "Italian Serie A (1)",
+        },
+        "Name": {
+            0: "Manchester City",
+            1: "Real Madrid",
+            2: "Liverpool",
+            3: "FC Bayern München",
+            4: "FC Barcelona",
+            5: "Juventus",
+        },
+        "TransferBudget": {0: 176000000, 1: 188500000, 2: 90000000, 3: 100000000, 4: 180500000, 5: 105000000},
+        "Unnamed: 0": {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5},
+    }
