feda0a500adc89de9445219ed33c607768e6de0d
Nurlan <nurlan.grachyov@mail.ru>
2024-10-09 07:18:17 +0400

    сортировка по статусу, ошибки

--- src/files.py
+++ src/files.py
@@ -17,23 +17,27 @@ fileHandler.setFormatter(fileFormatter)
 logger.addHandler(fileHandler)
 
 
-def read_file_csv(file):
+def read_file_csv(file, state='EXECUTED'):
     # try:
         logger.info("Получаем данные файла")
-        reader = read_csv(file)
-        dict_reader = reader.to_dict()
-        return dict_reader
-
+        with open(file, encoding='UTF-8') as f:
+            text = pd.read_csv(f)
+            new_text = text.to_dict(orient='records')
+            trans = text.loc[text.state == state]
+            print(trans)
+    # except Exception:
+    #     logger.error("Ошибка!")
+    #     return []
+        return trans
 
 
 def read_excel(file):
     try:
         logger.info("Получаем данные файла")
-        data_excel = pd.read_excel(file)
+        data_excel = read_excel(file)
         return data_excel
     except Exception:
         logger.error("Ошибка!")
-
         return []
 
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 
 from src.files import read_file_csv, PATH_TO_CSV, read_excel, PATH_TO_EXCEL
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 from src.utils import get_transactions, PATH_TO_FILE
 
 
@@ -21,18 +21,19 @@ def main():
             if user_input_file == "1":
                 print("Для обработки выбран JSON-файл.")
                 transactions_from_file = get_transactions(os.path.abspath(PATH_TO_FILE))
-                return transactions_from_file, 'JSON'
+                return transactions_from_file
             elif user_input_file == "2":
                 print("Для обработки выбран CSV-файл.")
                 transactions_from_file = read_file_csv(PATH_TO_CSV)
-                return transactions_from_file, 'CSV'
+                return transactions_from_file
             elif user_input_file == "3":
                 print("Для обработки выбран XLSX-файл.")
                 transactions_from_file = read_excel(PATH_TO_EXCEL)
-                return transactions_from_file, 'XLSX'
+                return transactions_from_file
             else:
                 print("Введен некорректный номер. Попробуйте заново.")
                 continue
+    get_transactions_from_file()
 
 
     def filter_by_state_main(transactions):
@@ -46,33 +47,35 @@ def main():
                 print(f"Статус операции {user_input_state} недоступен.")
                 continue
             print(f"Операции отфильтрованы по статусу {user_input_state}")
-            print(filter_by_state(transactions, user_input_state))
+            print(filter_by_state(transactions, user_input_state))  #УДАЛИТЬ
             break
+        return filter_by_state(transactions, user_input_state)
 
 
     print(filter_by_state_main(get_transactions_from_file()))
 
 
-#     while True:
-#         print("Отсортировать операции по дате? Да/Нет")
-#         user_input = input('Введите да или нет').lower()
-#         if user_input == 'да':
-#             print("Отсортировать по возрастанию или по убыванию?")
-#             user_input = input().lower()
-#             if user_input == 'по возрастанию':
-#                 pass
-#             elif user_input == 'по убыванию':
-#                 pass
-#             else:
-#                 print(f"Введен некорректный ответ. Попробуйте заново.")
-#                 continue
-#         elif user_input == 'нет':
-#             pass
-#         else:
-#             print(f"Введен некорректный ответ. Попробуйте заново.")
-#             continue
-#
-#
+    def sort_by_date_main():
+        while True:
+            print("Отсортировать операции по дате? Да/Нет")
+            user_input_date = input('Введите да или нет ').lower()
+            if user_input_date == 'да':
+                print("Отсортировать по возрастанию или по убыванию?")
+                user_input_up_down = input('в порядке убывания / в порядке возрастания ').lower()
+                if user_input_up_down == 'в порядке убывания' or user_input_up_down == 'в порядке возрастания':
+                    sort_by_date(filter_by_state_main(get_transactions_from_file()), user_input_date)
+                else:
+                    print(f"Введен некорректный ответ. Попробуйте заново.")
+                    continue
+            elif user_input_date == 'нет':
+                pass
+            else:
+                print(f"Введен некорректный ответ. Попробуйте заново.")
+                continue
+
+
+    print(sort_by_date_main())
+
 #         while True:
 #             print("Выводить только рублевые транзакции? Да/Нет")
 #             user_input = input('Введите да или нет').lower()
